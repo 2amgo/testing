fc9da511434bd67e60b4a96ebb8c52bda0563094
2AMgo <phucthinh.sun@gmail.com>
2022-01-03 10:37:59 +0700

    used recursion to demontrate

--- src/com/m2ago/mathutil/core/MathUtil.java
+++ src/com/m2ago/mathutil/core/MathUtil.java
@@ -4,19 +4,27 @@ package com.m2ago.mathutil.core;
 
 public class MathUtil {
   
+//    public static long getFactorial(int n) {
+//        
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("n must be between 0 .. 20");
+//        
+//        if (n == 0 || n == 1) 
+//            return 1;
+//        
+//        
+//        long product = 1;
+//        for (int i = 1; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//    }
     public static long getFactorial(int n) {
+        if (n > 20 || n < 0) 
+            throw new IllegalArgumentException("invalid agrument");
         
-        if (n < 0 || n > 20) 
-            throw new IllegalArgumentException("n must be between 0 .. 20");
-        
-        if (n == 0 || n == 1) 
+        if (n == 1)
             return 1;
-        
-        
-        long product = 1;
-        for (int i = 1; i <= n; i++) 
-            product *= i;
-        
-        return product;
-    }
+        return n * getFactorial(n-1);
+    } 
 }
